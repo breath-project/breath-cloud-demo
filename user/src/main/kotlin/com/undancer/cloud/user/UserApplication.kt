e5da13f58a0a2006f30c968eee5fee82354802ce
undancer <undancer@gmail.com>
2018-07-13 18:21:11 +0800

    将项目转为kotlin代码

---
+++ user/src/main/kotlin/com/undancer/cloud/user/UserApplication.kt
@@ -0,0 +1,20 @@
+package com.undancer.cloud.user
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cloud.client.SpringCloudApplication
+
+@SpringCloudApplication
+class UserApplication : SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder
+    }
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<UserApplication>(*args) {
+    }
+}
