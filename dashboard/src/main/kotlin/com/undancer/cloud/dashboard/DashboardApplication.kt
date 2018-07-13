c84b879a4dd8f0ca3a28ab30ede2295387a3eff1
undancer <undancer@gmail.com>
2018-07-13 20:06:17 +0800

    dashboard & 测试数据

---
+++ dashboard/src/main/kotlin/com/undancer/cloud/dashboard/DashboardApplication.kt
@@ -0,0 +1,32 @@
+package com.undancer.cloud.dashboard
+
+import com.undancer.cloud.dashboard.stream.MockStreamServlet
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.context.annotation.Bean
+
+
+@SpringBootApplication
+@EnableHystrixDashboard
+class DashboardApplication : SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder
+    }
+
+    @Bean
+    fun mockStreamServlet(): ServletRegistrationBean<*> {
+        return ServletRegistrationBean(MockStreamServlet(), "/mock.stream")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<DashboardApplication>(*args) {
+
+    }
+}
