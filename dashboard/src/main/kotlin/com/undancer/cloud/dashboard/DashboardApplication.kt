a909cae9074144cc1bbc6f3fc7c2112477b7cf3b
undancer <undancer@gmail.com>
2018-07-19 18:54:59 +0800

    注册在eureka中

--- dashboard/src/main/kotlin/com/undancer/cloud/dashboard/DashboardApplication.kt
+++ dashboard/src/main/kotlin/com/undancer/cloud/dashboard/DashboardApplication.kt
@@ -6,11 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.context.annotation.Bean
 
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
 class DashboardApplication : SpringBootServletInitializer() {
 
