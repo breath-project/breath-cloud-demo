b4cdec9aaa52de152559d7307c0029cd4331d408
undancer <undancer@gmail.com>
2018-07-21 00:36:25 +0800

    user使用config配置

---
+++ user/src/main/kotlin/com/undancer/cloud/user/EnvironmentController.kt
@@ -0,0 +1,23 @@
+package com.undancer.cloud.user
+
+import com.google.common.collect.Maps
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/env"])
+class EnvironmentController {
+
+    @Value("\${foo}")
+    private val username: String? = null
+
+    @GetMapping
+    fun home(): Any {
+        val resultObject = Maps.newHashMap<String, Any>()
+        resultObject["username"] = username
+        return resultObject
+    }
+
+}
