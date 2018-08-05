79a07f18d5a2d1f910b994917a936b0823518aa9
undancer <undancer@gmail.com>
2018-08-05 21:52:34 +0800

    转为kt代码

---
+++ gateway/src/main/kotlin/com/undancer/cloud/gateway/GatewayApplication.kt
@@ -0,0 +1,85 @@
+package com.undancer.cloud.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class GatewayApplication : SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder
+    }
+
+    @Bean
+    fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+                .route("path_route") { r ->
+                    r.path("/get")
+                            .uri("http://httpbin.org")
+                }
+                .route("host_route") { r ->
+                    r.host("*.myhost.org")
+                            .uri("http://httpbin.org")
+                }
+                .route("rewrite_route") { r ->
+                    r.host("*.rewrite.org")
+                            .filters { f ->
+                                f.rewritePath("/foo/(?<segment>.*)",
+                                        "/\${segment}")
+                            }
+                            .uri("http://httpbin.org")
+                }
+                .route("hystrix_route") { r ->
+                    r.host("*.hystrix.org")
+                            .filters { f -> f.hystrix { c -> c.name = "slowcmd" } }
+                            .uri("http://httpbin.org")
+                }
+                .route("hystrix_fallback_route") { r ->
+                    r.host("*.hystrixfallback.org")
+                            .filters { f -> f.hystrix { c -> c.setName("slowcmd").setFallbackUri("forward:/hystrixfallback") } }
+                            .uri("http://httpbin.org")
+                }
+                .route("limit_route") { r ->
+                    r
+                            .host("*.limited.org").and().path("/anything/**")
+                            .filters { f -> f.requestRateLimiter { c -> c.rateLimiter = redisRateLimiter() } }
+                            .uri("http://httpbin.org")
+                }
+                .route("websocket_route") { r ->
+                    r.path("/echo")
+                            .uri("ws://localhost:9000")
+                }
+
+                .route("rewrite_route_edit") { r ->
+                    r.alwaysTrue()
+                            .filters { f ->
+                                f.rewritePath("/foo/(?<segment>.*)",
+                                        "/\${segment}")
+                            }
+                            .uri("http://httpbin.org:3200")
+                }
+
+
+                .build()
+
+
+    }
+
+
+    @Bean
+    fun redisRateLimiter(): RedisRateLimiter {
+        return RedisRateLimiter(1, 2)
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args) {
+
+    }
+}
