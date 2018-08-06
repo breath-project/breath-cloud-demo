e163c1f6fac90afc2937993b45dbb1c2edf98f99
undancer <undancer@gmail.com>
2018-08-06 16:05:29 +0800

    使其可以被路由

--- user/src/main/kotlin/com/undancer/cloud/user/EnvironmentController.kt
+++ user/src/main/kotlin/com/undancer/cloud/user/EnvironmentController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping(path = ["/env"])
+@RequestMapping(path = ["/user/env"])
 class EnvironmentController {
 
     @Value("\${foo}")
