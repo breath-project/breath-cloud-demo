e83187eee73fea476d10b089072c244694562324
undancer <undancer@gmail.com>
2018-08-23 21:47:52 +0800

    zipkin通过web提交数据

--- services/user/src/main/kotlin/com/undancer/cloud/user/WelcomeController.kt
+++ services/user/src/main/kotlin/com/undancer/cloud/user/WelcomeController.kt
@@ -1,6 +1,5 @@
 package com.undancer.cloud.user
 
-import com.netflix.config.sources.URLConfigurationSource
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
